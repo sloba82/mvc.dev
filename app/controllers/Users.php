fcb5f5249eefd5e6e0c3bb1745adcdc1d1229aa1
sloba82 <dizajnistampa@gmail.com>
2020-09-20 17:50:26 +0200

    working on login validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Users extends Controller
 {
     public function __construct(){
@@ -9,22 +10,42 @@ class Users extends Controller
     //POST route /user/register
     public function register($post)
     {
-        $data = [
-            'name' => trim($post['name']),
-            'email' => trim($post['email']),
-            'password' => trim($post['password']),
-            'confirm_password' => trim($post['confirm_password']),
-            'name_err' => '',
-            'email_err' => '',
-            'password_err' => '',
-            'confirm_password_err' => ''
-        ];
+
+       
+
+        $data = [];
+        if (!empty($post)) {
+
+            $name_err ='';
+            $email_err = '';
+            $password_err = '';
+            $confirm_password_err = '';
+            
+            $data = [
+                'name' => trim($post['name']),   
+                'email' => trim($post['email']),
+                'password' => trim($post['password']),
+                'confirm_password' => trim($post['confirm_password']),
+                'name_err' => $name_err,
+                'email_err' => $email_err,
+                'password_err' =>  $password_err,
+                'confirm_password_err' => $confirm_password_err
+            ];
+
+            var_dump($data);
+        }
+        else {
+            $data = [];
+        }
+
+
 
         $this->view('users/register', $data);
     }
 
     public function login($post)
     {
+
         $data = [
             'email' => trim($post['email']),
             'password' => trim($post['password']),
---
+++ app/lib/Validator.php
@@ -0,0 +1,66 @@
+<?php
+
+
+class Validation
+{
+    /**
+     * Method for sanitizing input data.
+     *
+     * @param string $data
+     * @return string
+     */
+    public static function sanitizeData($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+
+        return $data;
+    }
+
+    /**
+     * Method for checking if input is numeric.
+     *
+     * @param $input
+     * @return int
+     */
+    public static function Numeric($input){
+        $exp = "/[0-9]+/";
+        return preg_match($exp,$input);
+    }
+
+    /**
+     * Method for checking if input has email format.
+     *
+     * @param $input
+     * @return int
+     */
+    public static function Email($input){
+        $exp = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/";
+        return preg_match($exp,$input);
+    }
+
+    /**
+     * Method for checking if input is only letters.
+     *
+     * @param $input
+     * @return int
+     */
+    public static function Alpha($input){
+
+        var_dump($input);
+        $exp = "/^[a-zA-Z]/";
+        return preg_match($exp,$input);
+    }
+
+    /**
+     * Method for checking if input is alphanumeric.
+     *
+     * @param $input
+     * @return int
+     */
+    public static function AlphaNumber($input){
+        $exp = "/^[a-zA-Z0-9]/";
+        return preg_match($exp,$input);
+    }
+}
